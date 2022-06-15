abbc5954dcb4b889e94427d008bb68a929663993
Jeremy Brown <jlbmagic@gmail.com>
2022-06-15 14:48:06 -0400

    set up dt and added two data sets

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-let table;
 // https://datatables.net/reference/option/
 // https://datatables.net/examples/index
 
+let table;
 window.loadTable = (json) => {
   const obj = JSON.parse(json);
-  // const data = obj.data;
-  const columns = [];
-  const data = [];
+  const data = obj.data;
+  const columnType = obj.columnType;
+
+  const customerColumns = [
+    { title: "Company", data: "fieldData.CompanyName" },
+    { title: "City", data: "fieldData.City" },
+    { title: "State", data: "fieldData.State" },
+    { title: "Zip", data: "fieldData.Zip" },
+  ];
+  const admissionsColumns = [
+    { title: "School", data: "fieldData.School" },
+    { title: "Program", data: "fieldData.Program" },
+    { title: "Grade", data: "fieldData.Grade" },
+  ];
+
+  const columns =
+    columnType === "Customers" ? customerColumns : admissionsColumns;
   table = $("#dtable").DataTable({
     columns,
     data,
+    scrollX: "100px",
+    pageLength: 20,
   });
 };
+
+// add a row click to the table rows
